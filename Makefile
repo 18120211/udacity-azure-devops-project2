1072c0e476c348394a00262b8e64eeb489a6a1be
Minh Vo <minhthevo123@gmail.com>
2022-11-27 22:37:41 +0700

    first pipeline run

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 setup:
 	python3 -m venv ~/.flask-ml-azure
-	#source ~/.flask-ml-azure/bin/activate
+	source ~/.flask-ml-azure/bin/activate
 	
 install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
 
 test:
-	#python -m pytest -vv --cov=myrepolib tests/*.py
-	#python -m pytest --nbval notebook.ipynb
+	# python -m pytest -vv --cov=myrepolib tests/*.py
+	# python -m pytest --nbval notebook.ipynb
 
 
 lint:
--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def predict():
         # Load pretrained model as clf. Try any one model. 
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        # clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+        clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
     except:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
